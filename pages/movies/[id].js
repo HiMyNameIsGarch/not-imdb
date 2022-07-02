da3d19194e4a6d40086ecbdfa61b15986ed04d54
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2022-07-02 22:13:23 +0300

    replace movie page css with Tailwind

--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -1,90 +1,81 @@
 import { useRouter } from 'next/router';
-import Image from 'next/image';
 import Head from 'next/head';
 import useSWR from 'swr';
 import { buildImageUrl } from '../../utils/api';
-import {
-  Badge,
-  Box,
-  Center,
-  CircularProgress,
-  Container,
-  Heading,
-  HStack,
-  Stack,
-  Tag,
-  Text,
-} from '@chakra-ui/react';
 import Layout from '../../components/Layout';
-import HistoryButton from '../../components/HistoryButton';
+// import HistoryButton from '../../components/HistoryButton';
 
+const Badge = ({ children }) => {
+    return (
+        <span className="inline-block border-2 border-solid border-pink-500 rounded-xl mr-2 text-xs font-semibold p-[0.3rem]">
+            {children}
+        </span>
+    );
+};
 const MovieContent = () => {
-  const { id } = useRouter().query;
-  const { data, error } = useSWR(id && `/api/movies/${id}`);
+    const { id } = useRouter().query;
+    const { data, error } = useSWR(id && `/api/movies/${id}`);
 
-  if (error) {
+    if (error) {
+        return (
+            <h1 className="bg-red-500">
+                Error fetching movie with ID {id}: {JSON.stringify(error)}
+            </h1>
+        );
+    }
+    if (!data) {
+        return <h1>In progress...</h1>;
+    }
+    const title = data.title;
+    if (data.tagline) {
+        title = title + ' - ' + data.tagline;
+    }
     return (
-      <Text color="red">
-        Error fetching movie with ID {id}: {JSON.stringify(error)}
-      </Text>
+        <div className="grid grid-cols-12 gap-4">
+            <Head>
+                <title>{data.title}</title>
+            </Head>
+            <div className="col-span-3">
+                <div className="min-w-[300px] relative">
+                    <img
+                        className="object-contain unoptimized"
+                        src={buildImageUrl(data.poster_path, 'w300')}
+                        alt="Movie Clip"
+                        width="300"
+                        height="450"
+                    />
+                </div>
+            </div>
+            <div className="col-span-9">
+                <div className="grid grid-cols-2 mb-2">
+                    <h1 className="col-span-1 text-3xl font-bold float-left">
+                        {title}
+                    </h1>
+                    <div className="col-span-1">
+                        <h1 className="float-right border-pink-500 rounded-xl border-solid border-2 p-[0.3rem]">
+                            {data.release_date}
+                        </h1>
+                    </div>
+                </div>
+                <div>
+                    {data.genres?.map((genre) => (
+                        <Badge key={genre.id}>{genre.name}</Badge>
+                    ))}
+                </div>
+                <div className="mt-3">
+                    <h1 className="text-base">{data.overview}</h1>
+                </div>
+            </div>
+        </div>
     );
-  }
-  if (!data) {
-    return (
-      <Center h="full">
-        <CircularProgress isIndeterminate />
-      </Center>
-    );
-  }
-  return (
-    <Stack direction={['column', 'row']} spacing={4}>
-      <Head>
-        <title>{data.title}</title>
-      </Head>
-      <Box minW="300px" pos="relative">
-        <HStack pos="absolute" zIndex={1} top={2} right={2}>
-          <HistoryButton />
-        </HStack>
-        <Image
-          src={buildImageUrl(data.poster_path, 'w300')}
-          alt="Movie poster"
-          layout="responsive"
-          width="300"
-          height="450"
-          objectFit="contain"
-          unoptimized
-        />
-      </Box>
-      <Stack>
-        <HStack justify="space-between">
-          <Heading as="h2">{data.title}</Heading>
-          <Box>
-            <Tag colorScheme="purple" variant="solid">
-              {data.release_date}
-            </Tag>
-          </Box>
-        </HStack>
-        <Box>{data.tagline}</Box>
-
-        <Stack direction="row">
-          {data.genres?.map((genre) => (
-            <Badge key={genre.id} colorScheme="purple" variant="outline">
-              {genre.name}
-            </Badge>
-          ))}
-        </Stack>
-        <Box>{data.overview}</Box>
-      </Stack>
-    </Stack>
-  );
 };
 
 export default function Movie() {
-  return (
-    <Layout>
-      <Container h="full">
-        <MovieContent />
-      </Container>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <div className="h-full w-full max-w-7xl mx-auto">
+                <MovieContent />
+            </div>
+        </Layout>
+    );
 }
