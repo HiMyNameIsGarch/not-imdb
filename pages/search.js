b7b7eaa2bc1ea1bd015330cb50b2e99dff2492dc
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2022-07-06 13:39:38 +0300

    move movie card into proper component

--- pages/search.js
+++ pages/search.js
@@ -1,9 +1,8 @@
-import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import useSWR from 'swr';
-
 import Layout from '../components/Layout';
+import MovieCard from '../components/MovieCard';
 
 function SearchBar() {
     const router = useRouter();
@@ -69,7 +68,6 @@ function SearchResults() {
     if (!data.results.length) {
         return <h1>:(((( No results </h1>;
     }
-    console.log(data.results);
     return (
         <div className="grid grid-cols-3 gap-4">
             {data.results.map(
@@ -81,35 +79,15 @@ function SearchResults() {
                     vote_average,
                     vote_count,
                 }) => (
-                    <div className="col-span-1" key={id}>
-                        <div className="font-bold py-3 px-6 border-b border-gray-300 text-gray-600 text-center">
-                            {vote_average +
-                                ' / 10 ' +
-                                ' with ' +
-                                vote_count +
-                                ' votes'}
-                        </div>
-                        <div className="block rounded-lg shadow-lg bg-white max-w-sm text-center">
-                            <div className="p-6">
-                                <Link href={`/movies/${id}`} passHref>
-                                    <h5
-                                        as="a"
-                                        variant="link"
-                                        className="text-gray-900 text-xl font-bold hover:underline hover:cursor-pointer mb-2"
-                                    >
-                                        {title}
-                                    </h5>
-                                </Link>
-                                <p className="text-gray-700 text-base mb-1">
-                                    {overview.slice(0, 75) || 'No description'}
-                                    ...
-                                </p>
-                            </div>
-                            <div className="font-bold py-3 px-6 border-t border-gray-300 text-gray-600">
-                                {release_date || 'No release date'}
-                            </div>
-                        </div>
-                    </div>
+                    <MovieCard
+                        key={id}
+                        id={id}
+                        title={title}
+                        overview={overview}
+                        release_date={release_date}
+                        vote_average={vote_average}
+                        vote_count={vote_count}
+                    />
                 ),
             )}
         </div>
